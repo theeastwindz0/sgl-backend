1effae5d387365fd640ff6e382e9971afe2e3be6
Himanshu Chauhan <developerhimanshuchauhan@gmail.com>
2023-07-07 23:37:41 +0530

    Changing image upload

--- router/movies.js
+++ router/movies.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'Zerox',
+    folder: 'sgl-miniplex',
   },
 });
 const upload = multer({ storage: storage });
